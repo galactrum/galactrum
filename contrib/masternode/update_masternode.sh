f68cc7df62735a70bbf07b93237fb3e2232e029c
shensu <shensu@protonmail.com>
2018-01-11 15:35:19 -0800

    change perms

--- contrib/masternode/update_masternode.sh
+++ contrib/masternode/update_masternode.sh
@@ -3,10 +3,10 @@
 wget https://github.com/galactrum/galactrum/releases/download/v1.1.5/galactrum-1.1.5-linux64.tar.gz
 sudo systemctl stop galactrumd
 tar -xvf galactrum-1.1.5-linux64.tar.gz
-mv galactrum-1.1.5/bin/galactrum{d,-cli} /usr/local/bin
-mv /home/masternode/.galactrum{,.bak}
+sudo mv galactrum-1.1.5/bin/galactrum{d,-cli} /usr/local/bin
+sudo mv /home/masternode/.galactrum /home/masternode/.galactrum.bak
 mkdir /home/masternode/.galactrum
-cp /home/masternode/.galactrum.bak/wallet.dat /home/masternode/.galactrum
-cp /home/masternode/.galactrum.bak/galactrum.conf /home/masternode/.galactrum
-chown -R masternode:masternode /home/masternode/.galactrum
+sudo cp /home/masternode/.galactrum.bak/wallet.dat /home/masternode/.galactrum
+sudo cp /home/masternode/.galactrum.bak/galactrum.conf /home/masternode/.galactrum
+sudo chown -R masternode:masternode /home/masternode/.galactrum
 sudo systemctl start galactrumd
