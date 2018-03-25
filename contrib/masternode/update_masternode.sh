1cc6fb1772ecc8ae4915ac90a0ccab25fc31d4fb
shensu <shensu@protonmail.com>
2018-03-25 11:33:51 -0700

    simplify update script

--- contrib/masternode/update_masternode.sh
+++ contrib/masternode/update_masternode.sh
@@ -4,9 +4,4 @@ wget https://github.com/galactrum/galactrum/releases/download/v1.1.6/galactrum-1
 sudo systemctl stop galactrumd
 tar -xvf galactrum-1.1.6-linux64.tar.gz
 sudo mv galactrum-1.1.6/bin/galactrum{d,-cli} /usr/local/bin
-sudo mv /home/masternode/.galactrum /home/masternode/.galactrum.bak
-mkdir /home/masternode/.galactrum
-sudo cp /home/masternode/.galactrum.bak/wallet.dat /home/masternode/.galactrum
-sudo cp /home/masternode/.galactrum.bak/galactrum.conf /home/masternode/.galactrum
-sudo chown -R masternode:masternode /home/masternode/.galactrum
 sudo systemctl start galactrumd
