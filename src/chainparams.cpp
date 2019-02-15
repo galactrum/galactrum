16393981b40758eb30d78195d5da3a360d3a1146
shensu <shensu@protonmail.com>
2019-02-14 18:59:14 -0800

    update testnet spork key

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -281,7 +281,7 @@ public:
 
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 5*60; // fulfilled requests expire in 5 minutes
-        strSporkPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+        strSporkPubKey = "04859dda6dfefa11b0f81a0f24622c2f22bd53d75d7cc071a46a69ceea0e2fe152c9fcb5e3a07c246af2fcb84ec7b5300a146766d6abf7b8dcd3d0ec5fd9e0dd29";
 
         checkpointData = {
             {
