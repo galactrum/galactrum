508791546e876b77c2a2d3a481bf3dce9bdd21d8
shensu <shensu@protonmail.com>
2019-02-18 15:24:00 -0800

    update testnet ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,9 +32,9 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 51473);
+        return MakeUnique<CBaseChainParams>("", 6269);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet3", 19998);
+        return MakeUnique<CBaseChainParams>("testnet3", 16269);
     else if (chain == CBaseChainParams::REGTEST)
         return MakeUnique<CBaseChainParams>("regtest", 18332);
     else
