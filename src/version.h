84fc5342649f51edfeb8561598691b74bb955cb6
shensu <shensu@protonmail.com>
2019-02-18 15:24:06 -0800

    update protocol

--- src/version.h
+++ src/version.h
@@ -9,9 +9,9 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70210;
+static const int PROTOCOL_VERSION = 70212;
 
-static const int PRESEGWIT_PROTO_VERSION = 70208;
+static const int PRESEGWIT_PROTO_VERSION = 70210;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
