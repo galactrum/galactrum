b1fd082a4063240ff224d8f33b440e6a38b507eb
shensu <shensu@protonmail.com>
2019-03-31 16:02:14 -0700

    add reindex helper script

---
+++ contrib/masternode/reindex_masternode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sudo systemctl stop galactrumd
+sudo sh -c "echo 'reindex=1' >> /home/masternode/.galactrum/galactrum.conf"
+sudo systemctl start galactrumd
+sleep 15
+sudo sed -i '/reindex/d' /home/masternode/.galactrum/galactrum.conf
